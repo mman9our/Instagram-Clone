eaae8339ab5e5e294290e0956ee4ba0dbf8df28d
mman9our <hack_225@outlook.sa>
2023-09-23 00:30:33 +0300

    fixed the issues

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,14 +1,14 @@
 import Suggestions from "../suggestions/Suggestions";
 
 function Timeline() {
-	<div className="timeline">
-		<div className="timeline__left">
-			<div className="timeline__posts"></div>
-		</div>
-		<div className="timeline__right">
-			<Suggestions />
-		</div>
-	</div>;
+  <div className="timeline">
+    <div className="home__left">
+      <div className="home__posts"></div>
+    </div>
+    <div className="home__right">
+      <Suggestions />
+    </div>
+  </div>;
 }
 
 export default Timeline;
--- src/timeline/Timeline.jsx
+++ src/timeline/Timeline.jsx
@@ -1,15 +1,10 @@
-import Suggestions from "../suggestions/Suggestions";
+// import Suggestions from "../components/suggestions/Suggestions";
 import "./Timeline.css";
 
 function Timeline() {
   return (
     <div className="timeline">
-      <div className="timeline__left">
-        <div className="timeline__posts">TimeLine</div>
-      </div>
-      <div className="timeline__right">
-        <Suggestions />
-      </div>
+     TimeLINE
     </div>
   );
 }
