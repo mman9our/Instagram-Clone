0ee716aea5ad268e6825ea9b9bb9e7fb6339385a
mman9our <hack_225@outlook.sa>
2023-09-22 21:13:56 +0300

    first commit

---
+++ src/Homepage.jsx
@@ -0,0 +1,16 @@
+import "./Homepage.css";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="homepage__navWraper">
+        {/* <Sidenav /> */}
+      </div>
+      <div className="homepage__timeline">
+        {/* <Timeline /> */}
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
