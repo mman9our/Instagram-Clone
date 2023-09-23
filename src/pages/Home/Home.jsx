6bdada520e66b6fc3b90ccc2103aaba77c8b4b67
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-09-23 11:03:57 +0300

    resolve conflicts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import "./Home.css";
-<<<<<<< HEAD
-import Timeline from "../../components/timeline/Timeline";
-=======
+
 import Suggestions from "../../components/suggestions/Suggestions";
 import Timeline from "../../timeline/Timeline";
 
->>>>>>> main
 function Home() {
   return (
     <>
