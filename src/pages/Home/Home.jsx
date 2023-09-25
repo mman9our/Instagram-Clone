73750e94a3622f3d99b8c3bc31398c7ee311f41d
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-09-25 15:09:33 +0300

    Create post feature is OUT NOW!

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,16 @@ import Suggestions from "../../components/suggestions/Suggestions";
 import Timeline from "../../components/timeline/Timeline";
 
 function Home() {
-  return (
-    <>
-      <div className="home__left">
-        <Timeline />
-      </div>
-      <div className="home__right">
-        <Suggestions />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="home__left">
+				<Timeline />
+			</div>
+			<div className="home__right">
+				<Suggestions />
+			</div>
+		</>
+	);
 }
 
 export default Home;
