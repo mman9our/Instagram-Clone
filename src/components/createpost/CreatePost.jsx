9a8cfcce3efa80348ad27928807fd002b673fa24
Ibraheem Shawhnee <116644664+IbraheemShawhnee@users.noreply.github.com>
2023-09-29 21:56:54 +0300

    Reimplement the modal to match the UI design

--- src/components/createpost/CreatePost.jsx
+++ src/components/createpost/CreatePost.jsx
@@ -3,94 +3,162 @@ import "./createpost.css";
 import Backdrop from "@mui/material/Backdrop";
 import PhotoOutlinedIcon from "@mui/icons-material/PhotoOutlined";
 import SmartDisplayOutlinedIcon from "@mui/icons-material/SmartDisplayOutlined";
-
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  color: "black",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "rgb(29, 29, 29)",
-  boxShadow: 24,
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	color: "black",
+	transform: "translate(-50%, -50%)",
+	bgcolor: "rgb(29, 29, 29)",
+	boxShadow: 24,
 };
-
 const Heading = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  fontSize: "16px",
-  color: "white",
-  fontWeight: "900",
-  padding: "6px",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	fontSize: "16px",
+	color: "white",
+	fontWeight: "900",
+	padding: "6px",
 };
-
 const CreatePost = ({ open, onClose }) => {
-  return (
-    <div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={onClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box className="modal" sx={style} width={"80%"} maxWidth="600px">
-            <Box
-              sx={{
-                borderBottom: "0.5px solid rgb(41, 41, 41)",
-                padding: 0,
-                margin: 0,
-              }}
-            >
-              <Typography
-                id="transition-modal-title"
-                variant="h6"
-                style={Heading}
-              >
-                Create new post
-              </Typography>
-            </Box>
+	return (
+		<div>
+			<Modal
+				aria-labelledby="transition-modal-title"
+				aria-describedby="transition-modal-description"
+				open={open}
+				onClose={onClose}
+				closeAfterTransition
+				slots={{ backdrop: Backdrop }}
+				slotProps={{
+					backdrop: {
+						timeout: 500,
+					},
+				}}
+			>
+				<Fade in={open}>
+					<Box
+						className="modal"
+						sx={style}
+						width={"38%"}
+						height={"80%"}
+						borderRadius={"20px"}
+					>
+						<Box
+							sx={{
+								borderBottom: "0.5px solid rgb(41, 41, 41)",
+								padding: 0,
+								margin: 0,
+							}}
+						>
+							<Typography
+								id="transition-modal-title"
+								variant="h6"
+								style={Heading}
+							>
+								Create new post
+							</Typography>
+						</Box>
 
-            <Grid container spacing={2} sx={{ mt: 2 }}>
-              <Grid item xs={12} sm={12}>
-                <Typography
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    height: "100%",
-                    color: "white",
-                    fontWeight: "900",
-                  }}
-                >
-                  Write post content here
-                  <div
-                    style={{
-                      margin: "75px 45px 45px 45px",
-                      background: "rgb(14, 104, 197)",
-                      borderRadius: "8px",
-                    }}
-                  >
-                    <Button sx={{ color: "white", padding: "8px" }}>
-                      Select from computer
-                    </Button>
-                  </div>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
-  );
+						<Typography
+							id="transition-modal-description"
+							sx={{
+								mt: 2,
+								color: "white",
+								fontWeight: 900,
+								padding: "40px",
+							}}
+						>
+							<Box className="inputTitle">
+								<div>Title</div>
+								<div>
+									<input
+										style={{
+											color: "white",
+											marginTop: "15px",
+											backgroundColor: "#4D4D4D",
+											width: "550px",
+											padding: "10px",
+											borderRadius: "10px",
+											border: "1px solid white",
+										}}
+									></input>
+								</div>
+							</Box>
+							<Box className="inputTitle" sx={{ marginTop: "30px" }}>
+								<div>Body</div>
+								<div>
+									<textarea
+										style={{
+											color: "white",
+
+											marginTop: "15px",
+											backgroundColor: "#4D4D4D",
+											width: "550px",
+											height: "200px",
+											borderRadius: "10px",
+
+											border: "1px solid white",
+										}}
+									></textarea>
+								</div>
+							</Box>
+							<Box className="inputTitle" sx={{ marginTop: "80px" }}>
+								<div>Image URL</div>
+								<div>
+									<input
+										style={{
+											color: "white",
+
+											marginTop: "15px",
+											backgroundColor: "#4D4D4D",
+											width: "550px",
+											padding: "10px",
+											borderRadius: "10px",
+											border: "1px solid white",
+										}}
+									></input>
+								</div>
+							</Box>
+							<div
+								style={{
+									marginTop: "30px",
+									alignItems: "center",
+									justifyContent: "center",
+									display: "flex",
+								}}
+							>
+								<Button
+									className="send-message-btn"
+									sx={{
+										fontWeight: 900,
+										paddingTop: "5px",
+										paddingBottom: "5px",
+										borderRadius: "10px",
+										fontSize: "14px",
+										color: "white",
+										backgroundColor: "rgb(14,127 ,244)",
+										":hover": {
+											bgcolor: "rgb(3, 90, 168)",
+											color: "white",
+										},
+										"&:active": {
+											boxShadow: "none",
+											backgroundColor: "#rgb(14,127 ,244)",
+											color: "white",
+										},
+									}}
+								>
+									Post{" "}
+								</Button>
+							</div>
+						</Typography>
+					</Box>
+				</Fade>
+			</Modal>
+		</div>
+	);
 };
 
 export default CreatePost;
